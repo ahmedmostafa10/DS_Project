7144d1efd9f1a191256852d6b4ce83166145d20a
ahmedmostafa10 <ahmedmostafa0522@gmail.com>
2026-05-03 20:55:37 +0300

    last

--- configs/models_grid_search_params.py
+++ configs/models_grid_search_params.py
@@ -2,27 +2,17 @@ MODEL_PARAMS = {
     "logistic_regression": {
         "penalty": ["l2"],
         "C": [0.01, 0.1, 1, 10],
-        "solver": ["lbfgs"]
+        "solver": ["lbfgs"],
     },
-
-    "svc": {
-        "C": [0.1, 10],
-        "kernel": ["rbf"]
-    },
-
-    "decision_tree": {
-        "max_depth": [5, 10, 20, None],
-        "min_samples_split": [2, 5, 10]
-    },
-
+    "svc": {"C": [0.1, 1, 10], "kernel": ["rbf"], "gamma": ["scale", "auto"]},
+    "decision_tree": {"max_depth": [5, 10, 20, None], "min_samples_split": [2, 5, 10]},
     "random_forest": {
-        "n_estimators": [100, 300,500, 1000],
-        "max_depth": [10, 20, None]
+        "n_estimators": [100, 300, 500, 1000],
+        "max_depth": [10, 20, None],
     },
-
     "xgboost": {
-        "n_estimators": [200, 300,500, 1000],
-        "max_depth": [3, 5,10,12],
-        "learning_rate": [0.05, 0.1]
-    }
+        "n_estimators": [200, 300, 500, 1000],
+        "max_depth": [3, 5, 10, 12],
+        "learning_rate": [0.05, 0.1],
+    },
 }
